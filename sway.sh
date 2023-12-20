31b152cc9303b993233771acf6c5f46cf0343c1d
Toru Ogawa <Hakuyume@users.noreply.github.com>
2023-12-20 23:03:30 +0900

    fcitx5 -> fcitx

--- sway.sh
+++ sway.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env sh
 
-export GTK_IM_MODULE=fcitx5
-export QT_IM_MODULE=fcitx5
-export XMODIFIERS=@im=fcitx5
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+export XMODIFIERS=@im=fcitx
 
 export QT_QPA_PLATFORM=wayland
 
