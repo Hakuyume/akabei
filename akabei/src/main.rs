dd0460ec413ccc3f0613528a67a96b6f617c6c0a
Toru Ogawa <Hakuyume@users.noreply.github.com>
2025-01-08 16:42:52 +0900

    package selector

--- akabei/src/main.rs
+++ akabei/src/main.rs
@@ -1,37 +1,27 @@
+use clap::Parser;
 use sha1::{Digest, Sha1};
 use std::env;
 use std::fs::{self, File};
 use std::io;
 use std::path::Path;
 
+#[derive(Parser)]
+struct Args {
+    #[clap(long, num_args = 1.., required = true)]
+    packages: Vec<String>,
+}
+
 fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
 
-    let mut packages = Vec::new();
-    for entry in walkdir::WalkDir::new(env::current_dir()?) {
-        let entry = entry?;
-        if entry.file_name() == concat!(env!("CARGO_BIN_NAME"), ".toml") {
-            let mut package =
-                toml::from_str::<package::Package>(&fs::read_to_string(entry.path())?)?;
-            for file in &mut package.files {
-                if file.source.is_relative() {
-                    file.source = entry
-                        .path()
-                        .parent()
-                        .ok_or_else(|| anyhow::format_err!("missing parent"))?
-                        .join(&file.source);
-                }
-                if file.target.is_relative() {
-                    file.target = dirs::home_dir()
-                        .ok_or_else(|| anyhow::format_err!("missing home_dir"))?
-                        .join(&file.target);
-                }
-            }
-            packages.push(package);
-        }
-    }
+    let args = Args::parse();
 
-    for package in &packages {
+    let packages = load_packages(env::current_dir()?)?;
+    for package_name in &args.packages {
+        let package = packages
+            .iter()
+            .find(|package| package.name == *package_name)
+            .ok_or_else(|| anyhow::format_err!("missing package `{package_name}`"))?;
         for file in &package.files {
             #[tracing::instrument(err, ret)]
             fn copy(source: &Path, target: &Path) -> io::Result<()> {
@@ -46,6 +36,39 @@ fn main() -> anyhow::Result<()> {
     }
 
     Ok(())
+}
+
+fn load_packages<P>(root: P) -> anyhow::Result<Vec<package::Package>>
+where
+    P: AsRef<Path>,
+{
+    #[tracing::instrument(err, ret)]
+    fn load(path: &Path) -> anyhow::Result<package::Package> {
+        let mut package = toml::from_str::<package::Package>(&fs::read_to_string(path)?)?;
+        for file in &mut package.files {
+            if file.source.is_relative() {
+                file.source = path
+                    .parent()
+                    .ok_or_else(|| anyhow::format_err!("missing parent"))?
+                    .join(&file.source);
+            }
+            if file.target.is_relative() {
+                file.target = dirs::home_dir()
+                    .ok_or_else(|| anyhow::format_err!("missing home_dir"))?
+                    .join(&file.target);
+            }
+        }
+        Ok(package)
+    }
+
+    let mut packages = Vec::new();
+    for entry in walkdir::WalkDir::new(root) {
+        let entry = entry?;
+        if entry.file_name() == concat!(env!("CARGO_BIN_NAME"), ".toml") {
+            packages.push(load(entry.path())?)
+        }
+    }
+    Ok(packages)
 }
 
 fn sha1<P>(path: P) -> io::Result<[u8; 20]>
@@ -81,7 +104,7 @@ mod state {
     use std::collections::BTreeMap;
     use std::path::PathBuf;
 
-    #[derive(Debug, Deserialize, Serialize)]
+    #[derive(Debug, Default, Deserialize, Serialize)]
     pub struct State {
         pub files: BTreeMap<PathBuf, File>,
     }
@@ -89,8 +112,12 @@ mod state {
     #[serde_with::serde_as]
     #[derive(Debug, Deserialize, Serialize)]
     pub struct File {
+        pub owner: String,
+        pub mode: u32,
         #[serde_as(as = "serde_with::hex::Hex")]
         pub sha1: [u8; 20],
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub post_install: Option<String>,
         #[serde(skip_serializing_if = "Option::is_none")]
         pub pre_remove: Option<String>,
     }
